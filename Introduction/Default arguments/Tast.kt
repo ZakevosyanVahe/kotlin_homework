64ea41a4a934d90cfcc7bf782e35a0b3dabf0680
ZakevosyanVahe <88457897+ZakevosyanVahe@users.noreply.github.com>
2021-08-06 16:45:25 +0200

    Update Tast.kt

--- Introduction/Default arguments/Tast.kt
+++ Introduction/Default arguments/Tast.kt
@@ -1,1 +1,13 @@
+fun foo(name: String, number: Int = 42, toUpperCase: Boolean = false) =
+    (if (toUpperCase) name.toUpperCase() else name) + number
 
+fun useFoo() = listOf(
+    foo("a"),
+    foo("b", number = 1),
+    foo("c", toUpperCase = true),
+    foo(name = "d", number = 2, toUpperCase = true)
+)
+
+fun main() {
+        useFoo().also { println(it) }
+}
