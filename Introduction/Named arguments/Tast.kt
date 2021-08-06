09280a82d0c5be2a70dda6123043fc9d1564104a
ZakevosyanVahe <88457897+ZakevosyanVahe@users.noreply.github.com>
2021-08-06 16:30:53 +0200

    Update Tast.kt

--- Introduction/Named arguments/Tast.kt
+++ Introduction/Named arguments/Tast.kt
@@ -1,1 +1,6 @@
+fun joinOptions(options: Collection<String>) =
+        options.joinToString(prefix = "[", postfix = "]")
 
+fun main() {
+        joinOptions(listOf("a","b","c")).also(::println)
+}
