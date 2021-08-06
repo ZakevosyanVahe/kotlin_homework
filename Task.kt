915780a0d86178aed54428fbf3855d1bbc016fe5
ZakevosyanVahe <88457897+ZakevosyanVahe@users.noreply.github.com>
2021-08-06 16:21:55 +0200

    Delete Task.kt

--- Task.kt
+++
@@ -1,6 +0,0 @@
-fun joinOptions(options: Collection<String>) =
-        options.joinToString(prefix = "[", postfix = "]")
-
-fun main() {
-        joinOptions(listOf("a","b","c")).also(::println)
-}
