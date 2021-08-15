a2e7173ae8fd4a98799e4d9741d9fdb919b48bdb
ZakevosyanVahe <88457897+ZakevosyanVahe@users.noreply.github.com>
2021-08-15 11:10:33 +0200

    Update Test.kt

--- homework/Conventions/Comparioson/Test.kt
+++ homework/Conventions/Comparioson/Test.kt
@@ -1,1 +1,18 @@
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
 
+    override fun compareTo(other: MyDate): Int {
+        if (this.year != other.year) {
+            return this.year - other.year
+        } else
+            if (this.month != other.month) {
+                this.month - other.month
+            }
+        return (this.dayOfMonth - other.dayOfMonth)
+
+    }
+}
+
+fun test(date1: MyDate, date2: MyDate) {
+    // this code should compile:
+    println(date1 < date2)
+}
