b6ce7a0bdf580dc1e74f9c8fd483be6a966bf8a3
ZakevosyanVahe <88457897+ZakevosyanVahe@users.noreply.github.com>
2021-08-15 11:27:42 +0200

    Create P35.kt

---
+++ homework/Git/Arithmetic/P35.kt
@@ -0,0 +1,28 @@
+package homework
+
+fun Int.primeFactors(): List<Int>{
+    val list = mutableListOf<Int>()
+    var i: Int = 2
+    var numero = this
+    while (i < this) {
+        if (i.isPrime()) {
+            if (numero % i == 0) {
+                list.add(i)
+                numero /=  i
+            } else {
+                i++
+            }
+        } else {
+            i++
+        }
+    }
+    if (numero != 1){
+        list.add(numero)
+    }
+
+    return list
+}
+
+fun main() {
+    println(111.primeFactors())
+}
