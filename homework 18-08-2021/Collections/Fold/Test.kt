17ea167dbaa35ccfc66a35d56d3d9dafadf4a5a0
ZakevosyanVahe <88457897+ZakevosyanVahe@users.noreply.github.com>
2021-08-21 17:25:46 +0200

    Create Test.kt

---
+++ homework 18-08-2021/Collections/Fold/Test.kt
@@ -0,0 +1,8 @@
+// Return the set of products that were ordered by all customers
+fun Shop.getProductsOrderedByAll(): Set<Product> {
+    val allProducts = customers.flatMap { it.getOrderedProducts() }.toSet()
+    return customers.fold(allProducts){a,b -> a.intersect(b.getOrderedProducts())}
+}
+
+fun Customer.getOrderedProducts(): List<Product> =
+        orders.flatMap { it.products }
